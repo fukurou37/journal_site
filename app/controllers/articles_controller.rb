6f3c2df3599f8c53ecee7811b995cb20a8f7f987
fukurou37 <swish3578@gmail.com>
2019-03-30 00:23:21 +0900

    追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,10 @@ class ArticlesController < ApplicationController
   def show
     # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
     # 取得したデータを@itemというインスタンス変数にセットする
-    @article = Article.find(params[:id])
+    @articles = Article.find(params[:id])
   end
+  def index
+    @articles = Article.all
+  end  
 
 end
